ebe55751db7227fd07ae685a682b2a1cb8f33724
Sudak_Yury <yury.sudak@gmail.com>
2022-11-01 21:48:11 +0300

    v0.2.1

--- src/main/java/zender/printer/Main.java
+++ src/main/java/zender/printer/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         int x = 5, y = 7;
         // instead of pluses:
-        // System.out.println("x = " + x + " y = " + y);
+        System.out.println("x = " + x + " y = " + y);
         // use commas
         Print.green("x =", x, "y =", y);
-        // you can choose another color: blue, red, white, purple, yellow, cian, gray
-        // or try random color
+        // you can choose this colors: blue, red, white, purple, yellow, cian, gray, green
+        // or try random color (233 color)
         Print.random("this text is colored with random color");
         // you can build colored line with defined colors
         Print.builder().blue("blue").green("green").yellow("yellow").build();
@@ -17,8 +17,10 @@ public class Main {
         Print.randomAll("all", "parts", "with", "random", "color");
         // you can print elements of array
         String[] arr = {"this", "is", "an", "array", "of", "Strings"};
+        Print.random(arr);
         Print.array(arr).blue();
         long[] l = {1L, 222222L, 3333333333333333L};
+        Print.random(l);
         Print.array(l).random();
         // even multidimansional array
         int[][] i = {{1,2},{3,4}};
